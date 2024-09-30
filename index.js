69a203d700c626cf1af4a549917d4c3271b2d3f3
christian guan <christianguan123@gmail.com>
2024-09-30 04:10:42 -0400

    update with newer version of inquirer

--- index.js
+++ index.js
@@ -1,70 +1,10 @@
-// Import required packages
-const fs = require('fs');
-const inquirer = require('inquirer');
-
-// Questions to generate the README
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a short description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install the project?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide instructions for using the project:',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'How can others contribute to the project?',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'How do you run tests for the project?',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Select the license for the project:',
-        choices: [
-            'MIT',
-            'Apache 2.0',
-            'GPL 3.0',
-            'BSD 3-Clause',
-            'BSD 2-Clause',
-            'LGPL 3.0',
-            'AGPL 3.0',
-            'MPL 2.0',
-            'Unlicense',
-            'None',
-        ],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:',
-    },
-];
+// Import required modules using ES Module syntax
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // Function to generate the README content
-const generateREADME = (answers) => `
+const generateREADME = (answers) => {
+  return `
 # ${answers.title}
 
 ## Description
@@ -73,36 +13,94 @@ ${answers.description}
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
+- [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [License](#license)
 - [Questions](#questions)
 
 ## Installation
+\`\`\`
 ${answers.installation}
+\`\`\`
 
 ## Usage
 ${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
 
 ## Contributing
 ${answers.contributing}
 
 ## Tests
+\`\`\`
 ${answers.tests}
-
-## License
-This project is licensed under the ${answers.license} license.
+\`\`\`
 
 ## Questions
-If you have any questions about the project, you can contact me directly at ${answers.email}. You can also find more of my work at [${answers.github}](https://github.com/${answers.github}).
-`;
+If you have any questions, you can contact me at:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `;
+};
 
-// Initialize the prompt
-inquirer.prompt(questions).then((answers) => {
+// Prompt the user for input
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+      default: 'npm install',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use this project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+      default: 'npm test',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ])
+  .then((answers) => {
     const readmeContent = generateREADME(answers);
 
     // Write the README file
-    fs.writeFile('generated/README.md', readmeContent, (err) =>
-        err ? console.error(err) : console.log('README.md has been generated!')
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
     );
-});
+  });
---
+++ readme.md
@@ -0,0 +1,38 @@
+
+# README Generator
+
+## Description
+uses node.js to create readme
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+```
+npm install
+```
+
+## Usage
+
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+
+
+## Tests
+```
+npm test
+```
+
+## Questions
+If you have any questions, you can contact me at:
+- GitHub: [chxdior](https://github.com/chxdior)
+- Email: christianguan123@gmail.com
+  
